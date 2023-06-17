aacaf5aacb58ec17e10b41cdc8368c244273ee0e
Ivan_merchan <ivanmerchan16@gmail.com>
2023-06-16 23:13:05 -0500

    first commit

---
+++ Models/DB_MerchanIvanContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Base_MerchanIvan.Models
+{
+    public partial class DB_MerchanIvanContext : DbContext
+    {
+        public DB_MerchanIvanContext()
+        {
+        }
+
+        public DB_MerchanIvanContext(DbContextOptions<DB_MerchanIvanContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Genero> Generos { get; set; } = null!;
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; DataBase=DB_MerchanIvan;Trusted_Connection=True; TrustServerCertificate=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genero>(entity =>
+            {
+                entity.HasNoKey();
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasNoKey();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Genero.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Base_MerchanIvan.Models
+{
+    public partial class Genero
+    {
+        public int? Id { get; set; }
+    }
+}
